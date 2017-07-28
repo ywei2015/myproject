954a5100e2cd8ec54211b9bd1f9d67ef76c7a32f
suncheng@talkweb.com.cn <suncheng@talkweb.com.cn@90ff4155-e805-44e8-8261-20b74613ee3e>
2017-07-28 07:11:39 +0000

    物料确认情况 controler

---
+++ TW20160928002/trunk/Java/batchproject/batchproject/src/sail/beans/controller/retrospected/MaterialUnacceptController.java
@@ -0,0 +1,70 @@
+package sail.beans.controller.retrospected;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import sail.beans.dao.iml.Pager;
+import sail.beans.entity.UnacceptMaterial;
+import sail.beans.entity.UnacceptMaterialDetail;
+import sail.beans.service.retrospect.MaterialUnacceptService;
+
+/*
+ * 未确认物资
+ */
+@Controller
+@RequestMapping("/MaterialUnaccept")
+public class MaterialUnacceptController {
+	@Resource
+	private MaterialUnacceptService materialUnacceptService;
+	/**
+	 * 获取未确认物资 
+	 */
+	@ResponseBody
+	@RequestMapping(value="/materialUnacceptBase")
+	public Map<String,Object> materialUnacceptBase(HttpServletRequest request){
+		Map map=new HashMap();
+		int pageSize = Integer.parseInt(request.getParameter("pageSize"));       //pageSize
+        int pageNumber = Integer.parseInt(request.getParameter("pageNumber"));   //pageNumber
+        String f_mat_code = request.getParameter("f_mat_code");					 //物料编码
+        String startDate = request.getParameter("startDate"); 					 //开始时间
+        String endDate = request.getParameter("endDate");						 //结束时间
+        Pager page=materialUnacceptService.materialUnacceptForBase(pageSize,pageNumber,f_mat_code,startDate,endDate);
+        
+		if(page!=null){
+			List<UnacceptMaterial> unacceptMaterialList=(List<UnacceptMaterial>) page.getList();
+			map.put("rows", unacceptMaterialList);
+			map.put("total", page.getRowsTotal());
+		}
+		return map;
+	}
+	
+	/**
+	 * 查看未确认物资大件明细
+	 */
+	@ResponseBody
+	@RequestMapping(value="/materialUnacceptDetail")
+	public Map<String,Object> materialUnacceptDetail(HttpServletRequest request){
+		Map map=new HashMap();
+		int pageSize = Integer.parseInt(request.getParameter("pageSize"));       //pageSize
+        int pageNumber = Integer.parseInt(request.getParameter("pageNumber"));   //pageNumber
+        String f_mat_code = request.getParameter("code");					 //物料编码
+        String date = request.getParameter("date"); 					 //开始时间
+        Pager page=materialUnacceptService.materialUnacceptForDetail(pageSize,pageNumber,f_mat_code,date);
+        
+		if(page!=null){
+			List<UnacceptMaterialDetail> unacceptMaterialDetailList=(List<UnacceptMaterialDetail>) page.getList();
+			map.put("rows", unacceptMaterialDetailList);
+			map.put("total", page.getRowsTotal());
+		}
+		
+		return map;
+	}
+}
